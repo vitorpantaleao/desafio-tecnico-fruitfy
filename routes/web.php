eb79f013578bbb4282979a1a3403bfcc772573c9
Vitor Pantaleão <vitor-hmp@hotmail.com>
2025-07-04 10:18:55 -0300

    Integrate Vue 3 with Inertia.js and Ziggy, update Vite configuration, and create main Blade template for the application.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\CreateContactController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('contacts.index');
 });
 
-Route::post('/contacts', CreateContactController::class);
+Route::resource('contacts', ContactController::class);
