e4c1c872ad1abf50c59595aebdc7ea189f1f2831
Vitor Pantaleão <vitor-hmp@hotmail.com>
2025-07-04 10:50:41 -0300

    Add Ziggy configuration file for route management and integration with Inertia.js

---
+++ resources/js/ziggy.js
@@ -0,0 +1,5 @@
+const Ziggy = {"url":"http:\/\/localhost:8000","port":8000,"defaults":{},"routes":{"contacts.index":{"uri":"contacts","methods":["GET","HEAD"]},"contacts.create":{"uri":"contacts\/create","methods":["GET","HEAD"]},"contacts.store":{"uri":"contacts","methods":["POST"]},"contacts.show":{"uri":"contacts\/{contact}","methods":["GET","HEAD"],"parameters":["contact"],"bindings":{"contact":"id"}},"contacts.edit":{"uri":"contacts\/{contact}\/edit","methods":["GET","HEAD"],"parameters":["contact"],"bindings":{"contact":"id"}},"contacts.update":{"uri":"contacts\/{contact}","methods":["PUT","PATCH"],"parameters":["contact"],"bindings":{"contact":"id"}},"contacts.destroy":{"uri":"contacts\/{contact}","methods":["DELETE"],"parameters":["contact"],"bindings":{"contact":"id"}},"storage.local":{"uri":"storage\/{path}","methods":["GET","HEAD"],"wheres":{"path":".*"},"parameters":["path"]}}};
+if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
+  Object.assign(Ziggy.routes, window.Ziggy.routes);
+}
+export { Ziggy };
