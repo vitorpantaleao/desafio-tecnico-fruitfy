d14c00ffff949673c428c139fcc7e954a1065e12
Vitor Pantaleão <vitor-hmp@hotmail.com>
2025-07-04 10:38:46 -0300

    Add @heroicons/vue dependency and update app.js to set default layout for pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,14 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { ZiggyVue } from 'ziggy-js'
 import { Ziggy } from './ziggy'
+import AppLayout from './Layouts/AppLayout.vue'
 
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    return pages[`./Pages/${name}.vue`]
+    const page = pages[`./Pages/${name}.vue`]
+    page.default.layout = page.default.layout || AppLayout
+    return page
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
